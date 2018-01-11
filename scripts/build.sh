961c1533d558975434324c78e960be4e0a032cd3
Luo Tao <lotreal@gmail.com>
2018-01-11 15:06:52 +0800

    FIX go build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-GOOS=linux GOARCH=amd64 go build -o build/greeter-server-linux-amd64 greeter_server/main.go
-GOOS=linux GOARCH=amd64 go build -o build/greeter-client-linux-amd64 greeter_client/main.go
+GOOS=linux GOARCH=amd64 go build -o build/greeter-server-linux-amd64 github.com/hg2c/hellogrpc/greeter_server
+GOOS=linux GOARCH=amd64 go build -o build/greeter-client-linux-amd64 github.com/hg2c/hellogrpc/greeter_client
+# GOOS=darwin GOARCH=amd64 go build -o build/greeter-server-darwin-amd64 github.com/hg2c/hellogrpc/greeter_server
+# GOOS=darwin GOARCH=amd64 go build -o build/greeter-client-darwin-amd64 github.com/hg2c/hellogrpc/greeter_client
